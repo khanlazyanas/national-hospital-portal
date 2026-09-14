f63e3208ed255904fa49bde50d943d8e3c76bf57
Anas Khan <anaskhan995620@gmail.com>
2026-09-14 15:34:24 +0530

    update image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         {/* Background Image & Gradient Overlay */}
         <div className="absolute inset-0 z-0">
           <img 
-            src="https://media.istockphoto.com/id/1686724303/photo/indian-female-gynecologist-doctor-consulting-young-married-couple-patients-in-fertility.jpg?s=2048x2048&w=is&k=20&c=28bW44aMXdAhFdVrJe4jC254oGmxNWjbag2v8Il8nnI=" 
+            src="https://images.unsplash.com/photo-1758691461957-474a7686e388?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODh8fGRvY3RvciUyMHByb2Zlc3Npb25hbCUyMGltYWdlfGVufDB8fDB8fHww" 
             alt="Medical Care Background" 
             className="w-full h-full object-cover object-center"
           />
